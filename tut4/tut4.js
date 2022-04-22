7d000232321a43cec12a4121bea4410632721f9a
Saurabh1Barasiya <saurabhbarasiya.sb@gmail.com>
2022-04-22 17:37:29 +0530

    About string length.

---
+++ tut4/tut4.js
@@ -0,0 +1,7 @@
+console.log("tut 4");
+let firstName = "Saurabh";
+console.log(firstName);
+console.log("0th index ",firstName[0]);
+console.log("last index value ",firstName[firstName.length-1]);
+console.log("length of string Saurabh ",firstName.length);
+console.log("length of string Barasiya","barasiya".length);
