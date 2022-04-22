0da2093c27a373b04f80a3bb3d568f4fe4e924cc
Saurabh1Barasiya <saurabhbarasiya.sb@gmail.com>
2022-04-22 16:48:53 +0530

    Use strict mode.

---
+++ tut2/tut2.js
@@ -0,0 +1,11 @@
+"use strict";
+console.log("this ia secound file");
+var firstName = "Aman";
+var firstName = "Raj";
+
+console.log(firstName);
+
+lastName = "Kumar";
+// we make varible as without word "var"
+// aesa na ho thats why we use "use strict"
+console.log(lastName);
