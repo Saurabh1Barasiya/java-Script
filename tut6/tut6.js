c1c1b7fe293d40e4bbe5af20a9304fef9001d004
Saurabh1Barasiya <saurabhbarasiya.sb@gmail.com>
2022-04-30 14:27:21 +0530

    type conversion number to string or string to number

---
+++ tut6/tut6.js
@@ -0,0 +1,25 @@
+console.log("Type converting.");
+let a = "22";
+let b = "2";
+let c = a + b;
+console.log("String h to aesa add",c);
+console.log(+a + +b);
+
+// String
+console.log("Integer here",typeof a);
+
+// Number
+console.log("Integer here",typeof +a);
+
+
+
+let e = "10";
+console.log("String to number",+e);
+console.log("String to number",Number(e));
+
+let f = 10;
+console.log("Number to string",String(f),typeof String(f));
+
+
+let g = f + "";
+console.log("Number to string",g,typeof g);
