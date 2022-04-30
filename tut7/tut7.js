b993a6d2d9d42e3b30ab6650435a8eab53f170c7
Saurabh1Barasiya <saurabhbarasiya.sb@gmail.com>
2022-04-30 17:37:51 +0530

    if else and equal and !===

---
+++ tut7/tut7.js
@@ -0,0 +1,29 @@
+let a = 5;
+console.log(`Wellcome to the world of ${a}`);
+
+// if -- else condition
+
+// if (a>=7){
+//     console.log(`${a} is greater`);
+// }else{
+//     console.log(`${a} is leser`);
+// }
+
+
+/*
+let b = "5";
+if (a===b){
+    console.log(`${a} and ${b} are same`); 
+}else{
+    console.log(`${a} and ${b} are not same`);
+}
+*/
+
+if(5!=="5"){
+    console.log("5 is not equal to 5");
+}else{
+    console.log("5 is equal to 5");
+}
+
+
+
