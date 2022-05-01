5ee366fc38094285212ef3545e581ccce0293f8a
Saurabh1Barasiya <saurabhbarasiya.sb@gmail.com>
2022-05-01 14:14:24 +0530

    Number Guessing game.

---
+++ tut9/tut9.js
@@ -0,0 +1,15 @@
+console.log("Hello World");
+// Number gassuing game
+// Language: javascript
+let winningNumber = 15;
+let guess = prompt("Guess a number between 1 and 20");
+if (guess == winningNumber) {
+  alert("You win!");
+} else {
+  if (guess > winningNumber) {
+    alert("Too high");
+  } else {
+    alert("Too low");
+  }
+}
+// Number guessing game
