2eea57ad2b0779d6b45c834ae80a2a8f2756ff07
Saurabh1Barasiya <saurabhbarasiya.sb@gmail.com>
2022-05-01 15:42:17 +0530

    Addtion program using while loop

---
+++ tut13/tut13.js
@@ -0,0 +1,9 @@
+console.log("Addtion program in javascript using while loop");
+
+let totel = 0;
+let i = 1;
+while (i <= 10) {
+    totel = totel + i;
+    i++;
+    }
+console.log("Addition of 1 to 10 is " + totel);
