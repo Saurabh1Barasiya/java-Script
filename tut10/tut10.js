0f9d6e85d203efe7218f76214095f1b5be6c3c8d
Saurabh1Barasiya <saurabhbarasiya.sb@gmail.com>
2022-05-01 14:32:42 +0530

    if-else-ladder.

---
+++ tut10/tut10.js
@@ -0,0 +1,14 @@
+console.log("else if ladder");
+
+let temperature = +prompt("What is the temperature?");
+if (temperature < 10) {
+    console.log("to cold were all nessarray shawter");
+}else if(temperature < 20){
+    console.log("normal cold were a sweater");
+}else if(temperature < 30){
+    console.log("Lets go to swim.");
+}else if(temperature < 40){
+    console.log("to cold were a coat");
+}else {
+    console.log("Very hot temperature");
+}
