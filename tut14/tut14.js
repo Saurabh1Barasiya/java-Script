0e1c933cdaf9c753d816aa37bf0c0bb5081e45cd
Saurabh1Barasiya <saurabhbarasiya.sb@gmail.com>
2022-05-01 16:09:36 +0530

    for loop and while loop and diffrence between let and var

---
+++ tut14/tut14.js
@@ -0,0 +1,13 @@
+console.log("foor loop");
+for (var i = 0; i < 10; i++) {
+    console.log(`-----${i}`);
+}
+console.log(i);
+
+// Here just check the difference between let and for loop.
+
+
+for (let j = 0; j < 10; j++) {
+    console.log(`-----${j}`);
+}
+console.log(j);
