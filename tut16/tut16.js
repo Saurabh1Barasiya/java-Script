b4729c4e9a38f799cbb4d8bf9b599121be94dff4
Saurabh1Barasiya <saurabhbarasiya.sb@gmail.com>
2022-05-01 17:18:03 +0530

    Do-while loop

---
+++ tut16/tut16.js
@@ -0,0 +1,14 @@
+console.log("Do while loop");
+let i = 1;
+do {
+    console.log(i);
+    i++;
+} while (false);
+
+console.log("=========");
+let j = 1;
+do {
+    console.log(j);
+    j++;
+} while (j <= 5);
+2<5;
