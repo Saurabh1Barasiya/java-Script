605dd1880968348eab2f7c50363912c174949bcd
Saurabh1Barasiya <saurabhbarasiya.sb@gmail.com>
2022-05-20 17:07:13 +0530

    Object function objectDestructuring({name, age,location}) {

---
+++ tut30/tut30.js
@@ -0,0 +1,16 @@
+console.log("Object destuctring ✔✔✔");
+
+const person = {
+    name: "John",
+    age: 30,
+    location: 'New York'
+};
+
+function objectDestructuring({name, age,location}) {
+   // maximum use in react application .
+
+    // const { name, age } = person;
+    console.log(`Name is ${name}, age is ${age} and location is ${location}`);
+}
+
+objectDestructuring(person);
