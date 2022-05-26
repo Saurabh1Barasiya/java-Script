87d1b727baf21294063085e88f2432fd4e0cf100
Saurabh1Barasiya <saurabhbarasiya.sb@gmail.com>
2022-05-26 16:42:16 +0530

    Closure in javascript

---
+++ tut58/tut58.js
@@ -0,0 +1,30 @@
+console.log("Closure in javascript");
+
+
+// 👀👀👀👀👀👀👀👀👀👀👀👀
+// agar kisi function ke ander koi function h or function function ko return kar raha h.
+
+// to return hone wala function apne saht outer function ke jo bhi variable h un sabko lekar return hota h.
+
+
+function outer() {
+    var name = "John";
+    function inner() {
+        console.log(name);
+    }
+    return inner;
+}
+
+const ans = outer();
+ans();
+
+
+
+function fun1(x){
+    return function fun2(y){
+        return x+y;
+    }
+}
+const ans1 = fun1(1);
+const ans2 = ans1(2);
+console.log(ans2);
