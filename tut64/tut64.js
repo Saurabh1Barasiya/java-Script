fdf6e072c28307f44120b46ec8651782efeb321a
Saurabh1Barasiya <saurabhbarasiya.sb@gmail.com>
2022-05-27 22:21:14 +0530

    addEventListener

---
+++ tut64/tut64.js
@@ -0,0 +1,7 @@
+console.log("know about event");
+
+const button = document.querySelector("button")
+// console.log(button);
+button.addEventListener("click",()=>{
+    console.log("you just click in button");
+})
