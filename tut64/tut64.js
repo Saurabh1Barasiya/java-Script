73c6038232b20643da283e4792182ef2eff88615
Saurabh1Barasiya <saurabhbarasiya.sb@gmail.com>
2022-05-27 22:50:48 +0530

    All about addEvetlistner

--- tut64/tut64.js
+++ tut64/tut64.js
@@ -1,7 +1,25 @@
 console.log("know about event");
 
+// this.
 const button = document.querySelector("button")
 // console.log(button);
-button.addEventListener("click",()=>{
+button.addEventListener("click",function(){
     console.log("you just click in button");
+    console.log(this);
+    // 👀👀👀👀👀👀👀👀👀👀👀👀👀👀👀👀👀👀👀👀
+    // it this case this ki value button hogi.
+
+    // but arrow function ke case me window object hogi.
+
 })
+
+
+const allbuttons = document.querySelectorAll(".container button");
+console.log("All buttons",allbuttons);
+
+for(let button of allbuttons){
+    button.addEventListener("click",function(){
+        console.log("you clicked me");
+        console.log(this.textContent);
+    })
+}
